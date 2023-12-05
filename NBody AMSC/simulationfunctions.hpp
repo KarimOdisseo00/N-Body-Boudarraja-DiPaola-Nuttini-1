28a264ef9e8037ebdc35a5efe8d50939964dea19
GDipa2000 <149886145+GDipa2000@users.noreply.github.com>
2023-12-05 16:08:14 +0100

    Update simulationfunctions.hpp
    major bugs fixed

--- NBody AMSC/simulationfunctions.hpp
+++ NBody AMSC/simulationfunctions.hpp
@@ -14,72 +14,101 @@
 
 class simulationfunctions{
 public:
-    
-    
-    // Metodo statico per generare una collezione di oggetti Particle
-    static std::vector<Particle<dim>> generateParticles() {
-        std::vector<Particle<dim>> particles;
-        //PER DEBUGGING
-        //particles.push_back(Particle<dim>(1, Arrows<dim>({1.0, 2.0, 3.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 5.0));
-        //particles.push_back(Particle<dim>(2, Arrows<dim>({3.0, 2.0, 1.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 7.0));
-        
-        // Metodo che genera un numero prefissato di particelle di dimensione DIM con valori casuali
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<double> distribution(-10.0, 10.0); // Cambia il range se necessario
-        std::uniform_real_distribution<double> massDistribution(0.1, 10.0); // Massa compresa tra 0.1 e 10.0 (valori arbitrari)
-
-        for (int i = 0; i < 1000000; ++i) {
-            // Genera valori casuali per posizione, velocità, massa, ecc.
-            Arrows<dim> randomPosition;
-            Arrows<dim> randomVelocity;
-            //Arrows<dim> randomAcceleration;
-            //Arrows<dim> randomCoefficients;
-
-            for (unsigned int j = 0; j < dim; ++j) {
-                randomPosition[j] = distribution(gen);
-                randomVelocity[j] = distribution(gen);
-                // Genera valori casuali per altre proprietà se necessario
-            }
-
-            double randomMass = massDistribution(gen);
-
-            // Crea e aggiungi una particella con valori casuali alla collezione
-            particles.push_back(Particle<dim>(i + 1, randomPosition, randomVelocity, Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0, 0.0, 0.0}), randomMass));
-        }
+  
+  
+   // Metodo statico per generare una collezione di oggetti Particle
+   static std::vector<Particle<dim>> generateParticles() {
+       std::vector<Particle<dim>> particles;
+       //PER DEBUGGING
+       //particles.push_back(Particle<dim>(1, Arrows<dim>({1.0, 2.0, 3.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 5.0));
+       //particles.push_back(Particle<dim>(2, Arrows<dim>({3.0, 2.0, 1.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 7.0));
+      
+       // Metodo che genera un numero prefissato di particelle di dimensione DIM con valori casuali
+       std::random_device rd;
+       std::mt19937 gen(rd());
+       std::uniform_real_distribution<double> distribution(-1000.0, 1000.0); // Cambia il range se necessario
+       std::uniform_real_distribution<double> massDistribution(0.1, 10.0); // Massa compresa tra 0.1 e 10.0 (valori arbitrari)
 
 
-        return particles;
-    }
+       for (int i = 0; i < 5; ++i) {
+           // Genera valori casuali per posizione, velocità, massa, ecc.
+           Arrows<dim> randomPosition;
+           Arrows<dim> randomVelocity;
+           //Arrows<dim> randomAcceleration;
+           //Arrows<dim> randomCoefficients;
 
-    //Metodo che esegue la simulazione. Tale metodo provvederà a chiamare il generatore di Particles.
-    static void executeSim(){
-        
-        std::vector<Particle<dim>> particles = std::vector<Particle<dim>>();
-        particles = generateParticles();
-        for(Particle particle : particles){
-            particle.setToZero();
-            particle.print();
-            std::cout<<" "<<std::endl;
-        }
-        std::cout<<"!!!! INIZIO DELLA SIMULAZIONE !!!!"<<std::endl;
-        std::cout<<" "<<std::endl;
-        for(unsigned int i = 0; i<particles.size(); ++i){
-            for(unsigned int j=0; j<particles.size(); ++j){
-                if(i==j){continue;}
-                particles[i].calcCoefficients(particles[i+1]);
-                particles[i].calcAccelleration();
-            }
-        }
-        double dt = 10;
-        for(Particle particle : particles){
-            particle.updateVelocity(dt);
-            particle.updatePosition(dt);
-            particle.print();
-            std::cout<<" "<<std::endl;
-        }
-    }
+
+           for (unsigned int j = 0; j < dim; ++j) {
+               randomPosition[j] = distribution(gen);
+               randomVelocity[j] = distribution(gen);
+               // Genera valori casuali per altre proprietà se necessario
+           }
+
+
+           double randomMass = massDistribution(gen);
+
+
+           // Crea e aggiungi una particella con valori casuali alla collezione
+           particles.push_back(Particle<dim>(i + 1, randomPosition, randomVelocity, Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0, 0.0, 0.0}), randomMass));
+       }
+
+
+
+
+       return particles;
+   }
+
+
+   //Metodo che esegue la simulazione. Tale metodo provvederà a chiamare il generatore di Particles. PER ORA SOLVER BASE!!!
+   static void doSim(){
+      
+       std::vector<Particle<dim>> particles = std::vector<Particle<dim>>();
+       particles = generateParticles();
+       for(Particle particle : particles){
+           particle.setToZero();
+           particle.print();
+           std::cout<<" "<<std::endl;
+       }
+       stepSim(particles);
+       updateSim(particles);
+  
+   }
+
+
+   static void stepSim(std::vector<Particle<dim>>& particles){ 
+       std::cout<<"!!!! INIZIO DELLA SIMULAZIONE !!!!"<<std::endl;
+       std::cout<<" "<<std::endl;
+      
+       for(unsigned int i = 0; i<particles.size(); ++i){
+           Arrows<dim> temp = Arrows<dim>();
+           for(unsigned int j=0; j<particles.size(); ++j){
+               if(i==j){continue;}
+               temp += particles[i].calcCoefficients(particles[j]);
+           }
+           particles[i].coefficientsSetter(temp);
+           particles[i].calcAccelleration();
+       }
+   }   
+      
+   static void updateSim(std::vector<Particle<dim>>& particles){
+       double dt = 10;
+       for(Particle particle : particles){
+           particle.updateVelocity(dt);
+           particle.updatePosition(dt);
+           particle.print();
+           std::cout<<" "<<std::endl;
+       }
+   }
+  
+ 
+   void solveCollisions(std::vector<Particle<dim>> particles){
+      
+   }
+
+
+  
 };
+
 
 #endif // PARTICLE_UTIL_HPP
 
